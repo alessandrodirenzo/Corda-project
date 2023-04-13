a7af5a1fc0ad6f3b43c8714d48907b17005aaa44
alessandrodirenzo <61700631+alessandrodirenzo@users.noreply.github.com>
2023-04-13 19:53:43 +0200

    Test of Contract, State and Flow correct

--- workflows/src/test/java/com/example/FlowTests.java
+++ workflows/src/test/java/com/example/FlowTests.java
@@ -1,16 +1,19 @@
 package com.example;
 
-import com.google.common.collect.ImmutableList;
 import com.example.flows.AskQuoteFlow;
+import com.example.flows.SendQuoteFlow;
 import com.example.states.Quote;
+import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.CordaX500Name;
-import net.corda.core.node.services.Vault;
-import net.corda.core.node.services.vault.QueryCriteria;
+import net.corda.core.identity.Party;
 import net.corda.core.transactions.SignedTransaction;
 import net.corda.testing.node.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import java.util.concurrent.Future;
 
@@ -18,15 +21,25 @@ public class FlowTests {
     private MockNetwork network;
     private StartedMockNode a;
     private StartedMockNode b;
-
+    private StartedMockNode c;
     @Before
     public void setup() {
-        network = new MockNetwork(new MockNetworkParameters().withCordappsForAllNodes(ImmutableList.of(
-                TestCordapp.findCordapp("com.example.contracts"),
-                TestCordapp.findCordapp("com.example.flows")))
-                .withNotarySpecs(ImmutableList.of(new MockNetworkNotarySpec(CordaX500Name.parse("O=Notary,L=London,C=GB")))));
-        a = network.createPartyNode(null);
-        b = network.createPartyNode(null);
+        MockNetworkParameters mockNetworkParameters = new MockNetworkParameters().withCordappsForAllNodes(
+                Arrays.asList(
+                        TestCordapp.findCordapp("com.example.contracts"),
+                        TestCordapp.findCordapp("com.example.flows")
+                )
+        ).withNotarySpecs(Arrays.asList(new MockNetworkNotarySpec(new CordaX500Name("Notary", "London", "GB"))));
+        network = new MockNetwork(mockNetworkParameters);
+        a = network.createNode(new MockNodeParameters());
+        b = network.createNode(new MockNodeParameters());
+        c = network.createNode(new MockNodeParameters());
+        ArrayList<StartedMockNode> startedNodes = new ArrayList<>();
+        startedNodes.add(a);
+        startedNodes.add(b);
+        startedNodes.add(c);
+        startedNodes.forEach(el -> el.registerInitiatedFlow(AskQuoteFlow.AskQuoteFlowResponder.class));
+        startedNodes.forEach(el -> el.registerInitiatedFlow(SendQuoteFlow.SendQuoteFlowResponder.class));
         network.runNetwork();
     }
 
@@ -36,13 +49,24 @@ public class FlowTests {
     }
 
     @Test
-    public void dummyTest() {
-        //Modify it
-        AskQuoteFlow.AskQuoteFlowInitiator flow= new AskQuoteFlow.AskQuoteFlowInitiator(a.getInfo().getLegalIdentities().get(0), b.getInfo().getLegalIdentities().get(0));
+    public void AskQuoteCorrectDemandingOfQuote() throws Exception {
+
+        Party sender = a.getInfo().getLegalIdentitiesAndCerts().get(0).getParty();
+        Party receiver = b.getInfo().getLegalIdentitiesAndCerts().get(0).getParty();
+        AskQuoteFlow.AskQuoteFlowInitiator flow = new AskQuoteFlow.AskQuoteFlowInitiator(sender, receiver);
+
         Future<SignedTransaction> future = a.startFlow(flow);
         network.runNetwork();
-        //successful query means the state is stored at node b's vault. Flow went through.
-        QueryCriteria inputCriteria = new QueryCriteria.VaultQueryCriteria().withStatus(Vault.StateStatus.UNCONSUMED);
-        Quote state = b.getServices().getVaultService().queryBy(Quote.class,inputCriteria).getStates().get(0).getState().getData();
+
+
+        SignedTransaction ptx = future.get();
+
+        // Print the transaction for debugging purposes.
+        System.out.println(ptx.getTx());
+        assert (ptx.getTx().getInputs().isEmpty());
+        assert (ptx.getTx().getOutputs().get(0).getData() instanceof Quote);
+        assert ( ((Quote) ptx.getTx().getOutputs().get(0).getData()).getQuote()==-1);
+
     }
+    
 }
